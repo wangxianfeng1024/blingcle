3c359ebab49495202f651731816a479b2285ffe1
a540 <1696648557@qq.com>
2018-07-04 17:49:22 +0800

    用户信息接口开发

--- src/main/java/com/blingcle/common/found/buser/vo/VideoVo.java
+++ src/main/java/com/blingcle/common/found/buser/vo/VideoVo.java
@@ -216,4 +216,3 @@ public class VideoVo {
         this.circleid = circleid;
     }
 }
-}
---
+++ src/main/java/com/blingcle/common/found/pojo/Systemparameter.java
@@ -0,0 +1,115 @@
+package com.blingcle.common.found.pojo;
+
+import java.util.Date;
+
+public class Systemparameter {
+    private Long id;
+
+    private String name;
+
+    private String value;
+
+    private String illustrate;
+
+    private Date createtime;
+
+    private Boolean isvalid;
+
+    private String createby;
+
+    private String updateby;
+
+    private Date updatetime;
+
+    private String modificy;
+
+    private Integer version;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value == null ? null : value.trim();
+    }
+
+    public String getIllustrate() {
+        return illustrate;
+    }
+
+    public void setIllustrate(String illustrate) {
+        this.illustrate = illustrate == null ? null : illustrate.trim();
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public Boolean getIsvalid() {
+        return isvalid;
+    }
+
+    public void setIsvalid(Boolean isvalid) {
+        this.isvalid = isvalid;
+    }
+
+    public String getCreateby() {
+        return createby;
+    }
+
+    public void setCreateby(String createby) {
+        this.createby = createby == null ? null : createby.trim();
+    }
+
+    public String getUpdateby() {
+        return updateby;
+    }
+
+    public void setUpdateby(String updateby) {
+        this.updateby = updateby == null ? null : updateby.trim();
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public String getModificy() {
+        return modificy;
+    }
+
+    public void setModificy(String modificy) {
+        this.modificy = modificy == null ? null : modificy.trim();
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+}
